808be8dc2f6bf25af791b25161477479f07faff6
loginexists <bxye@stanford.edu>
2021-11-08 21:47:18 -0800

    input one-hot encoding

--- src/models/train_structured.py
+++ src/models/train_structured.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
 
 def get_structured_data_path():
     """
@@ -46,9 +47,10 @@ def run_DNN_model(x_train, y_train, x_test, y_test, epochs, batch_size):
     
     # define model
     model = Sequential()
-    model.add(Dense(20, activation='relu', kernel_initializer='he_normal', input_shape=(x_train.shape[1],)))
-    model.add(Dense(12, activation='relu', kernel_initializer='he_normal'))
-    model.add(Dense(10, activation='relu', kernel_initializer='he_normal'))
+    model.add(Dense(81, activation='relu', kernel_initializer='he_normal', input_shape=(x_train.shape[1],)))
+    model.add(Dense(32, activation='relu', kernel_initializer='he_normal'))
+    model.add(Dense(16, activation='relu', kernel_initializer='he_normal'))
+    model.add(Dense(8, activation='relu', kernel_initializer='he_normal'))
     model.add(Dense(5, activation='softmax'))
     # compile the model
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -62,24 +64,29 @@ def run_DNN_model(x_train, y_train, x_test, y_test, epochs, batch_size):
 if __name__ == "__main__":
     raw_structured_data = pd.read_csv(os.path.join(get_structured_data_path(), "hotel_meta_processed.csv"))
     # input
-    x = raw_structured_data[["openyear", "numReviewers", "roomRating", "serviceRating",
+    x1 = raw_structured_data[["openyear", "numReviewers", "roomRating", "serviceRating",
                             "value4moneyRating", "locatioRating", "roomquantity",
                             "minArea", "isStarValidated", "order_cnt", "roomnight_cnt",
                             "avg_room_price", "avg_person_price", "maxprice", "minprice",
-                            "adr1", "adr2", "adr3", "adr4", "adr5", "gym", "executive_lounge",
-                            "indoor_swimming_pool", "bathrobe", "laundry_service", "X24h_frontdesk",
-                            "conference_hall", "luggage_storage", "roomcleaneddaily",
-                            "outdoor_swimming_pool", "avg_person_price_bycity", "pic_hq_ratio",
+                            "adr1", "adr2", "adr3", "adr4", "adr5", "avg_person_price_bycity", "pic_hq_ratio",
                             "pic_apperance_ratio", "pic_public_ratio", "pic_meeting_ratio",
                             "pic_restaurant_ratio", "pic_leisure_ratio", "pic_service_ratio",
                             "is_adr2_adjusted", "is_adr3_adjusted",
                             "is_adr4_adjusted", "is_adr5_adjusted"]]
+    x2 = raw_structured_data[["renovationyear", "gym", "executive_lounge",
+                            "indoor_swimming_pool", "bathrobe", "laundry_service", "X24h_frontdesk",
+                            "conference_hall", "luggage_storage", "roomcleaneddaily", "outdoor_swimming_pool"]]
+    
+    encoder = OneHotEncoder(handle_unknown='error')
+    x2_onehot = pd.DataFrame(encoder.fit_transform(x2).toarray())
+
+    x = pd.concat([x1, x2_onehot], axis=1)
 
     # output
     y = raw_structured_data["star"]
 
     # split data into train and test
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=123)
 
     #handling null(if any)
     x_train = np.nan_to_num(x_train)
